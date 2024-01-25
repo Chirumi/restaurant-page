66fb705664b103285fb981cd49902ae0ebb8f153
michiru <kirittoes@gmail.com>
2024-01-25 17:18:56 +0000

    Change headline

--- src/home.js
+++ src/home.js
@@ -4,7 +4,7 @@ function home() {
     const contentContainer = document.createElement("div")
 
     const headline = document.createElement("h1")
-    headline.textContent = "Savor the Flavor: Indulge in Gourmet Bliss at out Signature Burger Prince"
+    headline.textContent = "Indulge in Flavour: Discover Burger Prince's Signature Burgers!"
     
     const description = document.createElement("em")
     description.textContent = "Welcome to Burger Prince, where each bite is a symphony of flavors crafted with culinary mastery. Our burgers redefine indulgence, featuring premium ingredients and expertly grilled patties"
