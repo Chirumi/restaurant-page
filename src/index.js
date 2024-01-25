3f4acd41f86349c31a0938f041faae09666701de
michiru <kirittoes@gmail.com>
2024-01-25 17:33:29 +0000

    Changed mode to development

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import home from "./home.js"
 import "./styles/styles.css"
 
+const menuBtn = document.querySelector("menuBtn")
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.textContent = "CHICKEN"
+})
+
 
