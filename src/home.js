edefb6184ed494c89638f3bf83bc8b53457e6cb0
michiru <kirittoes@gmail.com>
2024-01-25 13:48:00 +0000

    Add image on homepage and style both header and footer hover underline effect

--- src/home.js
+++ src/home.js
@@ -1,19 +1,20 @@
 function home() {
     const content = document.getElementById("content")
 
-    const headline = document.createElement("h2")
+    const contentContainer = document.createElement("div")
+
+    const headline = document.createElement("h1")
     headline.textContent = "Savor the Flavor: Indulge in Gourmet Bliss at out Signature Burger Prince"
     
     const description = document.createElement("em")
     description.textContent = "Welcome to Burger Prince, where each bite is a symphony of flavors crafted with culinary mastery. Our burgers redefine indulgence, featuring premium ingredients and expertly grilled patties"
 
-    const burgerImage = document.createElement("img")
+    contentContainer.appendChild(headline)
+    contentContainer.appendChild(description)
+    content.prepend(contentContainer)
 
-    content.appendChild(headline)
-    content.appendChild(description)
-
-    headline.classList.add("headline", "fadeIn", "fadeOut")
-    description.classList.add("description", "fadeIn", "fadeOut")
+    headline.classList.add("fadeIn", "fadeOut")
+    description.classList.add("fadeIn", "fadeOut")
 
     requestAnimationFrame(() => {
         headline.classList.remove("fadeOut")
