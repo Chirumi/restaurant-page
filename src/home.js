21ee8775b334844daf916eff8edece9aeecaadf8
michiru <kirittoes@gmail.com>
2024-01-25 12:47:21 +0000

    Finish header styles and add fade in animation on homepage

--- src/home.js
+++ src/home.js
@@ -2,15 +2,23 @@ function home() {
     const content = document.getElementById("content")
 
     const headline = document.createElement("h2")
-    headline.classList.add("headline")
     headline.textContent = "Savor the Flavor: Indulge in Gourmet Bliss at out Signature Burger Prince"
-
+    
     const description = document.createElement("em")
-    description.classList.add("description")
     description.textContent = "Welcome to Burger Prince, where each bite is a symphony of flavors crafted with culinary mastery. Our burgers redefine indulgence, featuring premium ingredients and expertly grilled patties"
+
+    const burgerImage = document.createElement("img")
 
     content.appendChild(headline)
     content.appendChild(description)
+
+    headline.classList.add("headline", "fadeIn", "fadeOut")
+    description.classList.add("description", "fadeIn", "fadeOut")
+
+    requestAnimationFrame(() => {
+        headline.classList.remove("fadeOut")
+        description.classList.remove("fadeOut")
+    })
 }
 
 export default home()
