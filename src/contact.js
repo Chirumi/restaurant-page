e8093d156df0592b8ce7157d6e8a95486713410b
michiru <kirittoes@gmail.com>
2024-01-30 22:39:07 +0000

    Add img of map and text elements to contacts page

--- src/contact.js
+++ src/contact.js
@@ -1,24 +1,33 @@
 function contact() {
     const contactContainer = document.createElement("div")
+    contactContainer.classList.add("contacts")
 
+    const locImgWrapper = document.createElement("div")
+    locImgWrapper.classList.add("locImgWrapper")
     const locationImg = document.createElement("img")
+    locationImg.src = "../src/photos/locationMap.jpg"
+
+    const contactInfo = document.createElement("div")
+    contactInfo.classList.add("contactInfo")
 
     const openHrs = document.createElement("div")
     const monFri = document.createElement("div")
-    monFri.textContent = "Monday - Friday: 9am - 11:30pm"
+    monFri.innerHTML = "Monday - Friday: <b>9am - 11:30pm</b>"
     const satSun = document.createElement("div")
-    satSun.textContent = "Saturday & Sunday: 8:30am - 12am"
+    satSun.innerHTML = "Saturday & Sunday: <b>8:30am - 12am</b>"
     const contactNum = document.createElement("div")
-    contactNum.textContent = "Call +00 0123456789"
+    contactNum.innerHTML = "Call <b>+00 0123456789</b>"
     const contactEmail = document.createElement("div")
     contactEmail.textContent = "restaurantPage@restaurantPage.com"
 
     openHrs.appendChild(monFri)
     openHrs.appendChild(satSun)
-    contactContainer.appendChild(locationImg)
-    contactContainer.appendChild(openHrs)
-    contactContainer.appendChild(contactNum)
-    contactContainer.appendChild(contactEmail)
+    contactInfo.appendChild(openHrs)
+    contactInfo.appendChild(contactNum)
+    contactInfo.appendChild(contactEmail)
+    locImgWrapper.appendChild(locationImg)
+    contactContainer.appendChild(locImgWrapper)
+    contactContainer.appendChild(contactInfo)
 
     return contactContainer
 }
