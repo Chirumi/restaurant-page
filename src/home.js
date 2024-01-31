4265a911b0833d536e49a63e6d5bbe8679a0aa64
michiru <kirittoes@gmail.com>
2024-01-31 16:08:05 +0000

    Change headline text

--- src/home.js
+++ src/home.js
@@ -4,7 +4,7 @@ function home() {
 
     const headlineDescWrapper = document.createElement("div")
     const headline = document.createElement("h1")
-    headline.textContent = "CHICKEN in Flavour: Discover Burger Prince's Signature Burgers!"
+    headline.textContent = "Indulge in Flavour: Discover Burger Prince's Signature Burgers!"
     const description = document.createElement("em")
     description.textContent = "Welcome to Burger Prince, where each bite is a symphony of flavors crafted with culinary mastery. Our burgers redefine indulgence, featuring premium ingredients and expertly grilled patties"
     
